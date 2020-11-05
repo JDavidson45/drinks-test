b669facdf18b27043dec565aa958aa48bb50018c
Justin Davidson <justindavidson45@gmail.com>
2020-11-05 15:18:15 -0600

    working

--- server/index.js
+++ server/index.js
@@ -100,7 +100,7 @@ const createApp = () => {
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
   const server = app.listen(PORT, () =>
-    console.log(`Mixing it up on port ${PORT}`)
+    console.log(`Mixing it up on port ${process.env.PORT || PORT}`)
   )
 
   // set up our socket control center
